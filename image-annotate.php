a78ae3085525012fc11087a801ceb962ba78e72d
Giuseppe Sciacca <g.sciacca@glauco.it>
2020-06-23 17:34:52 +0200

    fallback to static file in frontend

--- image-annotate.php
+++ image-annotate.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name: Image Annotator
   Description: Adds the ability to create annotated images and then edit the annotations later.
@@ -6,16 +7,18 @@
   Author: Moe Loubani
   Author URI: http://www.moeloubani.com
   License: GPLv3
+  GitHub Plugin URI: seed-glauco/image-annotator
+  GitHub Plugin URI: https://github.com/seed-glauco/image-annotator
  */
 
 
-define('VANILLA_TAGGER_EDITOR_CSS_FILE',__DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.css' );
-define('VANILLA_TAGGER_NAVIGATION_CSS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/navigation/vanilla-tagger-navigation.css' );
-define('VANILLA_TAGGER_THEME_CSS_FILE', __DIR__ . '/lib/vanilla-tagger/vanilla-tagger.theme.css' );
-define('VANILLA_TAGGER_EDITOR_TAGDATA_TMPL_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.tagdata.tmpl.js' );
-define('VANILLA_TAGGER_EDITOR_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.webc.js' );
-define('VANILLA_TAGGER_NAVIGATION_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/navigation/vanilla-tagger-navigation.webc.js' );
-define('VANILLA_TAGGER_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/vanilla-tagger.webc.js' );
+define( 'VANILLA_TAGGER_EDITOR_CSS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.css' );
+define( 'VANILLA_TAGGER_NAVIGATION_CSS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/navigation/vanilla-tagger-navigation.css' );
+define( 'VANILLA_TAGGER_THEME_CSS_FILE', __DIR__ . '/lib/vanilla-tagger/vanilla-tagger.theme.css' );
+define( 'VANILLA_TAGGER_EDITOR_TAGDATA_TMPL_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.tagdata.tmpl.js' );
+define( 'VANILLA_TAGGER_EDITOR_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/editor/vanilla-tagger-editor.webc.js' );
+define( 'VANILLA_TAGGER_NAVIGATION_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/plugins/navigation/vanilla-tagger-navigation.webc.js' );
+define( 'VANILLA_TAGGER_WEBC_JS_FILE', __DIR__ . '/lib/vanilla-tagger/vanilla-tagger.webc.js' );
 
 
 
