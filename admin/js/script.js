0f094201aef753de8bf3624c1fe527f1ed5aa284
Giuseppe Sciacca <g.sciacca@glauco.it>
2020-06-23 11:26:21 +0200

    add ini data-tags

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,26 +1,20 @@
-"use strict";
-
-(function () {
-    //alert("admin JS loaded");
-})();
-
-
 jQuery(document).ready(function () {
-    jQuery('#upload_image_button').click(function () {
-        tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
-        return false;
-    });
-    window.send_to_editor = function (html) {
-        var imgurl, srcCheck = jQuery(html).attr('src');
-        if (srcCheck && typeof srcCheck !== 'undefined') {
-            imgurl = srcCheck;
-        } else {
-            imgurl = jQuery('img', html).attr('src');
-        }
-        jQuery('#upload_image').val(imgurl);
-        jQuery('#wpia-preview-image').attr('src', imgurl);
-
-        //console.log(imgurl);
-        tb_remove();
+  jQuery("#upload_image_button").click(function () {
+    tb_show("", "media-upload.php?type=image&amp;TB_iframe=true");
+    return false;
+  });
+  window.send_to_editor = function (html) {
+    var imgurl,
+      srcCheck = jQuery(html).attr("src");
+    if (srcCheck && typeof srcCheck !== "undefined") {
+      imgurl = srcCheck;
+    } else {
+      imgurl = jQuery("img", html).attr("src");
     }
+    jQuery("#upload_image").val(imgurl);
+    jQuery("#wpia-preview-image").attr("src", imgurl);
+
+    //console.log(imgurl);
+    tb_remove();
+  };
 });
