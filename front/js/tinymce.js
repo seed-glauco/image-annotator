a3565e12ee42a3b7d92f802e6fc53bfcd58d82a6
Giuseppe Sciacca <g.sciacca@glauco.it>
2020-06-25 12:25:40 +0200

    optimized shortcode button

--- front/js/tinymce.js
+++ front/js/tinymce.js
@@ -1,28 +1,46 @@
-(function() {
+(function () {
     tinymce.create('tinymce.plugins.annotate', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('annotate', {
-                title : 'Annotated Image',
-                image : url+'/images/pencil.png',
-                onclick: function() {
-                    ed.windowManager.open( {
+                title: 'Annotated Image',
+                image: url + '/images/pencil.png',
+                onclick: function () {
+                    console.log(annotations);
+                    ed.windowManager.open({
                         title: 'Choose Annotated Image',
+
                         body: [
                             {
                                 type: 'listbox',
                                 name: 'annotatedImage',
                                 label: 'Annotated Images',
-                                'values': annotations
+                                values: annotations
+                            },
+                            {
+                                type: 'listbox',
+                                name: 'navigatorStatus',
+                                label: 'Navigator',
+                                values: [
+                                    {text: 'No', value: '0'},
+                                    {text: 'Yes', value: '1'},
+                                ]
                             }
                         ],
-                        onsubmit: function( e ) {
-                            ed.insertContent( '[wpia_image id="' + e.data.annotatedImage + '"]');
+                        onsubmit: function (e) {
+                            var placeholderstring='';
+                            if(e.data.navigatorStatus=="1"){
+                                placeholderstring='placeholder="navigator-placeholder-'+e.data.annotatedImage+'"';
+                            }
+                            ed.insertContent('[wpia_image id="' + e.data.annotatedImage + '" navigator="'+e.data.navigatorStatus+'" '+placeholderstring+']');
+                            if(e.data.navigatorStatus=="1"){
+                                ed.insertContent('<div id="navigator-placeholder-'+e.data.annotatedImage+'"></div>');
+                            }                            
                         }
                     });
                 }
             });
         },
-        createControl : function(n, cm) {
+        createControl: function (n, cm) {
             return null;
         },
     });
