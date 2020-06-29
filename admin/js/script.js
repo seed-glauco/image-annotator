7b72436b88dea9ba79595b7233283d82e5568c0e
Giuseppe Pizzimenti <giuseppe.pizzimenti@gmail.com>
2020-06-29 15:22:03 +0200

    WP Media Library dialog

--- admin/js/script.js
+++ admin/js/script.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function () {
   let $tagger = jQuery("#wpia-preview-image");
 
   jQuery("#upload_image_button").click(function () {
+    document.querySelector("body").classList.add("WPMLOpen");
     tb_show("", "media-upload.php?type=image&amp;TB_iframe=true");
     return false;
   });
@@ -13,7 +14,24 @@ jQuery(document).ready(function () {
     //jQuery("#image_annotation_json").text(JSON.stringify(e.detail));
   });
 
+  var original_tb_remove = window.tb_remove;
+
+  window.tb_remove = function () {
+    document.querySelector("body").classList.remove("WPMLOpen");
+
+    var dialog = document.getElementById("vt-dialogForm");
+    if (dialog && dialog.open) {
+      let field = dialog.querySelector(".updating");
+      if (field) {
+        field.classList.remove("updating");
+      }
+    }
+    original_tb_remove();
+  };
+
   window.send_to_editor = function (html) {
+    var dialog = document.getElementById("vt-dialogForm");
+
     var imgurl,
       srcCheck = jQuery(html).attr("src");
     if (srcCheck && typeof srcCheck !== "undefined") {
@@ -21,10 +39,20 @@ jQuery(document).ready(function () {
     } else {
       imgurl = jQuery("img", html).attr("src");
     }
-    jQuery("#upload_image").val(imgurl);
-    $tagger.attr("src", imgurl);
+
+    if (dialog && dialog.open) {
+      let field = dialog.querySelector(".updating");
+      if (field) {
+        field.value = imgurl;
+        field.classList.remove("updating");
+      }
+    } else {
+      jQuery("#upload_image").val(imgurl);
+      $tagger.attr("src", imgurl);
+    }
 
     //console.log(imgurl);
-    tb_remove();
+    document.querySelector("body").classList.remove("WPMLOpen");
+    original_tb_remove();
   };
 });
