9d7ca37ec8e46654ca70a1f11644c67618b1eea9
Giuseppe Pizzimenti <giuseppe.pizzimenti@gmail.com>
2020-06-30 12:20:19 +0200

    scroll tag into view only on  nav item click

--- image-annotate.php
+++ image-annotate.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: Image Annotator
   Description: Adds the ability to create annotated images and then edit the annotations later.
-  Version: 1.4.4
+  Version: 1.4.5
   Author: Moe Loubani
   Author URI: http://www.moeloubani.com
   License: GPLv3
